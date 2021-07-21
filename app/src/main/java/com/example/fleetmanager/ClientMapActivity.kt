727267c7a7d53e4166e61ee72b2d1f511a64876c
rachier-alal <rachiermark285@gmail.com>
2021-07-21 12:00:56 +0300

    starting

---
+++ app/src/main/java/com/example/fleetmanager/ClientMapActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fleetmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClientMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_map)
+    }
+}
